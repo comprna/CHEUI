fe04d2ce0d960eaae95575ce8aa7ffea102b3a19
pabloacera <pabloacera91@gmail.com>
2021-05-11 13:38:42 +1000

    updating predict_CHEUI_model_1.py

--- scripts/predict_CHEUI_model_1.py
+++ scripts/predict_CHEUI_model_1.py
@@ -12,19 +12,14 @@ parser = argparse.ArgumentParser(prog='predict_MILONGAS v0.1', description=
                                  """ 
                                  This script takes a ID and signal files and generate predictions every motif with and A
                                  
-                                 """, usage='python predict_MILONGAS.py -s <path_to_signals_file> -i <path_to_ID_file> '\
+                                 """, usage='python predict_MILONGAS.py  -i <path_to_signlas_IDs_file> '\
                                             '-m <path_to_DL_model> -o <file_out> \nversion: %(prog)s')
 
 OPTIONAL = parser._action_groups.pop()
 REQUIRED = parser.add_argument_group('required arguments')
 
 
-REQUIRED.add_argument("-s", "--signals_input",
-                      help="path to the signal file",
-                      metavar='\b',
-                      required=True)
-
-REQUIRED.add_argument("-i", "--ids_input",
+REQUIRED.add_argument("-i", "--signals_input",
                       help="path to the IDs file",
                       metavar='\b',
                       required=True)
@@ -49,7 +44,6 @@ ARGS = parser.parse_args()
 
 # required arg
 signals_input = ARGS.signals_input
-ids_input = ARGS.ids_input
 DL_model = ARGS.DL_model
 file_out = ARGS.file_out
 
@@ -72,48 +66,49 @@ model.load_weights(DL_model)
 
 ### load the stored data 
 counter = 0
-IDs = []
-signals = []
+IDs_signals = {}
+
 with open(signals_input, 'rb') as signal_in:
-    with open(ids_input, 'rb') as id_in:
-        while True:
-            try:
-                counter +=1
-                IDs.append(cPickle.load(id_in))
-                signals.append(cPickle.load(signal_in))
-                # to avoid loading everything predict every 10k singnals
-                if counter%20000 == 0:
-                    print(counter, 'signals predicted')
-                    predictions = model.predict(np.array(signals))
-                    predictions_df = pd.DataFrame.from_dict({'KMER': IDs,
-                                                            'Prediction': predictions.reshape(len(predictions)).tolist()}
-                                                            )
-                    
-                    predictions_df.to_csv(file_out,
-                                          mode='a',
-                                          header=False,
-                                          sep='\t', 
-                                          index=False)
-                    IDs = []
-                    signals = []
-                    predictions_df = pd.DataFrame({'KMER': [], 'Prediction': []})
-            except:
-                if IDs:
-                    predictions = model.predict(np.array(signals))
-                    predictions_df = pd.DataFrame.from_dict({'KMER': IDs,
-                                                            'Prediction': predictions.reshape(len(predictions)).tolist()}
-                                                            )
-                    
-                    predictions_df.to_csv(file_out,
-                                          mode='a',
-                                          header=False,
-                                          sep='\t', 
-                                          index=False)
-                    IDs = []
-                    signals = []
-                    predictions_df = pd.DataFrame({'KMER': [], 'Prediction': []})
-                print('All signals have been processed')
-                break
+    while True:
+        try:
+            counter +=1
+            IDs_signals.update(cPickle.load(signal_in))
+            # to avoid loading everything predict every 10k singnals
+            if counter%20000 == 0:
+                print(counter, 'signals predicted')
+                print(IDs_signals.values())
+                predictions = model.predict(np.array(list(IDs_signals.values())))
+                
+                predictions_df = pd.DataFrame.from_dict({'KMER': IDs_signals.keys(),
+                                                        'Prediction': predictions.reshape(len(predictions)).tolist()}
+                                                        )
+                
+                predictions_df.to_csv(file_out,
+                                      mode='a',
+                                      header=False,
+                                      sep='\t', 
+                                      index=False)
+                IDs_signals = {}
+
+                predictions_df = pd.DataFrame({'KMER': [], 'Prediction': []})
+        except:
+            if IDs_signals:
+               
+                predictions = model.predict(np.array(list(IDs_signals.values())))
+                predictions_df = pd.DataFrame.from_dict({'KMER': IDs_signals.keys(),
+                                                        'Prediction': predictions.reshape(len(predictions)).tolist()}
+                                                        )
+                
+                predictions_df.to_csv(file_out,
+                                      mode='a',
+                                      header=False,
+                                      sep='\t', 
+                                      index=False)
+                IDs_signals = {}
+
+                predictions_df = pd.DataFrame({'KMER': [], 'Prediction': []})
+            print('All signals have been processed')
+            break
 
 
 
