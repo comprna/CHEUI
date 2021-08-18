1b94c2254491945c63ff7ffc2eca07a8329ce090
pabloacera <pabloacera91@gmail.com>
2021-08-18 10:40:30 +1000

    Add files via upload

---
+++ scripts/CHEUI_diffenrentialRNAMod.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 12 10:05:12 2021
+
+@author: labuser
+"""
+
+import argparse
+import yaml
+from scipy.stats import ranksums
+import itertools
+import statsmodels.api as sm
+import numpy as np
+
+parser = argparse.ArgumentParser(prog='CHEUI_compare v0.1', description=
+                                 """ 
+                                 This script takes all predictions at single read level and site level and reported 
+                                 differential methylation sites \
+                                 
+                                 
+                                 """, usage='python CHEUI_compare.py -c'\
+                                            '<config_file> \nversion: %(prog)s')
+
+    
+OPTIONAL = parser._action_groups.pop()
+REQUIRED = parser.add_argument_group('required arguments')
+
+
+REQUIRED.add_argument("-c", "--config_file",
+                      help="path to the config file",
+                      metavar='\b',
+                      required=True)
+
+OPTIONAL.add_argument('-v', '--version', 
+                        action='version', 
+                        version='%(prog)s')
+
+ARGS = parser.parse_args()
+
+# required arg
+config_file = ARGS.config_file
+
+with open(config_file, 'r') as stream:
+    try:
+        config_dic = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print('error at reading yaml file')
+        print(exc)
+
+input_file = config_dic['input']
+output_file = config_dic['out']
+
+condition1 = list(config_dic['sample_labels']['condition1'].values())
+condition2 = list(config_dic['sample_labels']['condition2'].values())
+lower_cutoff=config_dic['lower_cutoff']
+upper_cutoff=config_dic['upper_cutoff']
+
+def run_tests(predictions_site):
+    '''
+    Parameters
+    ----------
+    predictions_site : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    '''
+    try:
+        condition1_values = [predictions_site[i] for i in condition1]
+        condition2_values = [predictions_site[i] for i in condition2]
+    except:
+        return False
+    
+    condition1_values_f = list(itertools.chain.from_iterable(condition1_values))
+    condition2_values_f = list(itertools.chain.from_iterable(condition2_values))
+    
+    if len(condition1_values_f) > 20 and len(condition2_values_f) > 20:
+        
+        mod1 = [i for i in condition1_values_f if i > upper_cutoff]
+        no_mod1 = [i for i in condition1_values_f if i < lower_cutoff]
+        
+        mod2 = [i for i in condition2_values_f if i > upper_cutoff]
+        mo_mod2 = [i for i in condition2_values_f if i < lower_cutoff]
+        
+        U1, p = ranksums(condition1_values_f,
+                         condition2_values_f)
+        
+        y = len(condition1_values_f)*[1]+\
+             len(condition2_values_f)*[0]
+                                 
+        logit_model=sm.Logit(y,condition1_values_f+condition2_values_f)
+        result=logit_model.fit(disp=0)
+        
+        return [len(condition1_values_f), 
+                len(condition2_values_f),
+                len(mod1)/(len(mod1)+len(no_mod1)),
+                len(mod2)/(len(mod2)+len(mo_mod2)),
+                U1,  np.mean([p, result.pvalues[0]])]
+       
+    else:
+        return False
+
+
+counter = 0
+predictions_site = {}
+with open(output_file, 'w') as file_out:
+    with open(input_file, 'r') as f:
+        
+        print('ID'+'\t'+'coverage_1'+\
+              '\t'+'coverage_2'+'\t'+'stoichiometry_1'+\
+              '\t'+'stoichiometry_2'+\
+              '\t'+'Z-sore'+
+              '\t'+'combined_pval', file=file_out)
+        
+        for line in f:
+            counter +=1
+            line_p = line.rstrip().split('\t')
+            if counter == 1:
+                predictions_site[line_p[-1]] = [float(line_p[1])]
+                ID = '_'.join(line_p[0].split('_')[:-1])
+            else:
+                if ID != '_'.join(line_p[0].split('_')[:-1]):
+                    results = run_tests(predictions_site)
+                    if results is not False:
+                         print(ID+'\t'+'\t'.join(str(x) for x in results),
+                               file=file_out)
+                         # Empty dictionary
+                         predictions_site = {}
+                         
+                    if line_p[-1] in predictions_site:
+                        predictions_site[line_p[-1]] += [float(line_p[1])]
+                    else:
+                        predictions_site[line_p[-1]] = [float(line_p[1])]
+    
+                    ID = '_'.join(line_p[0].split('_')[:-1])
+                    counter +=1
+                else:
+                    if line_p[-1] in predictions_site:
+                        predictions_site[line_p[-1]] += [float(line_p[1])]
+                    else:
+                        predictions_site[line_p[-1]] = [float(line_p[1])]
+                        
+                    ID = '_'.join(line_p[0].split('_')[:-1])
+        if predictions_site:
+            results = run_tests(predictions_site)
+            if results is not False:
+                print(ID+'\t'+'\t'.join(str(x) for x in results),
+                      file=file_out)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
